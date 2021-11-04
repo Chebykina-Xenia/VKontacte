146f5331a2ab23f83117303207ddd0e69b3a3ce4
chebykina <chebykina.xenia@gmail.com>
2021-11-04 15:26:19 +0300

    Доработка менеджера

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -4,16 +4,20 @@ import ru.netology.domain.Post;
 
 public class PostManager {
     private Post[] posts;
+
     //выводим массив
     public Post[] getPosts() {
+
         return null;
     }
 
     //удаление записи со стены
-    public void deletePost(idAvtor, idPost){}
+    public void deletePost(int idAvtor, int idPost) {
+    }
 
     //поиск записи по заданным критериям
-    public Post[] searchPost(idAvtor)  {
+    public Post[] searchPost(int idAvtor, String domain, String qyery) {
+
         return null;
     }
 }
